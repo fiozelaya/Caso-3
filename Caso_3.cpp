724bd58729055cea9bc357adcbf836fdc7aea08d
fiozelaya <fiorella.zelaya1024@gmail.com>
2022-04-10 11:06:34 -0600

    first commit

--- Caso_3.cpp
+++ Caso_3.cpp
@@ -1,1 +1,38 @@
+/*
 
+Caso 3 
+Fiorella Zelaya
+Ericka Yu Min Guo Chen
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Punto
+{
+public:
+	Punto() = default;
+
+
+private:
+	int x;
+    int y;
+};
+
+
+class Path
+{
+public:
+    Path() = default;
+
+
+private:
+    string id;
+    string opacity;
+    string fill; //color
+    string datos; //puntos y líneas
+    string posInicial;
+    string posDestino;
+}
